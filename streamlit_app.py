ad0e8db72ddb284712b7ddcc3db67c2d6881ecc6
ipmist1979 <127585675+ipmist1979@users.noreply.github.com>
2024-10-02 05:15:29 +0000

    tugas 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Hallo World 🫦")
+
+name = st.text_input("Name :")
+    
+
+if(name):
+        st.write('Halo,', name)
+else:
+        st.warning("isi nama!!!")
